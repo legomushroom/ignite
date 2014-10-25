5fac7af5c82fef417234dcf891d0fd062e293dcb
Oleg Solomka <legomushroom@gmail.com>
2014-10-25 05:16:04 +0200

    add 3 embers

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
     this.o = o || {};
     this.ctx = this.o.ctx;
     this.top = this.o.top;
+    this.color = this.o.color || 'deeppink';
     this.p = 0; // used to animate delta
     if (!this.ctx) { console.error('no context, aborting'); return}
     this.getFlickBounds()
@@ -21,18 +22,17 @@
   };
 
   Ember.prototype.draw = function Draw(){
-    this.ctx.clearRect(0,0,1200,1200)
     this.ctx.beginPath();
-    this.ctx.moveTo(200*PX, 300*PX);
+    this.ctx.moveTo(250*PX, 300*PX);
     var topX = this.top.x+(this.p*this.delta.x);
     var topY = this.top.y+(this.p*this.delta.y);
     this.ctx.lineTo(topX*PX, topY*PX);
-    this.ctx.lineTo(400*PX, 300*PX);
+    this.ctx.lineTo(350*PX, 300*PX);
     this.ctx.closePath();
-    this.ctx.fillStyle = 'rgba(255,20,147,0.5)';
+    this.ctx.fillStyle = this.color;
     this.ctx.fill();
 
-    this.p += .2;
+    this.p += this.o.sensivity;
     if (this.p >= 1) {
       this.top.x = topX;
       this.top.y = topY;
@@ -55,7 +55,7 @@
       x: this.top.x,
       y: this.top.y,
     }
-    var flickRadius = 20*PX; // !!hardCode!!
+    var flickRadius = 10*PX; // !!hardCode!!
 
     this.flickCenter = flickCenter;
     this.flickRadius = flickRadius;
@@ -64,8 +64,8 @@
   Ember.prototype.getDelta = function(){
     var angle = rand(0,360);
     var newTop = {
-      x: Math.round(this.flickCenter.x + Math.cos(angle*DEG)*this.flickRadius),
-      y: Math.round(this.flickCenter.y + Math.sin(angle*DEG)*2*this.flickRadius)
+      x: this.flickCenter.x + Math.cos(angle*DEG)*.5*this.flickRadius,
+      y: this.flickCenter.y + Math.sin(angle*DEG)*1.5*this.flickRadius
     }
     // console.log(newTop);
     var delta = {
@@ -75,20 +75,36 @@
     return delta;
   }
 
-
   var ember = new Ember({
       ctx: ctx,
-      top: { x: 250, y: 100 }
+      sensivity: .15,
+      color: 'rgba(255,20,147,0.5)',
+      top: { x: 275, y: 100 }
+    });
+
+  var ember2 = new Ember({
+      ctx: ctx,
+      sensivity: .15,
+      color: 'rgba(155,20,147,0.5)',
+      top: { x: 325, y: 150 }
+    });
+
+  var ember3 = new Ember({
+      ctx: ctx,
+      sensivity: .1,
+      color: 'rgba(55,120,147,0.5)',
+      top: { x: 300, y: 50 }
     });
   
   // LOOP
   var loop = function loop(){
+    ctx.clearRect(0,0,1200,1200)
     ember.draw();
+    ember2.draw();
+    ember3.draw();
     requestAnimationFrame(loop)
   };
   loop();
-
-
 
 
 
